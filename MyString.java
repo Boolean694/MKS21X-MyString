8ef32d01856510ded60c744b9608c5ce07ce6f4f
Julian Wong <jwong01@stuy.edu>
2018-11-23 00:09:19 -0500

    finish writing and able to compile

--- MyString.java
+++ MyString.java
@@ -4,6 +4,7 @@ public class MyString implements CharSequence,Comparable<CharSequence> {
         return data.length;
     }
     public char charAt(int g) {
+		if(g < 0 || g >= this.length()){throw new IndexOutOfBoundsException();}
         return data[g];
     }
     public String toString() {
@@ -14,22 +15,41 @@ public class MyString implements CharSequence,Comparable<CharSequence> {
         return s;
     }
     public CharSequence subSequence(int s, int e) {
-        char[] n = new char[(e - s) + 1];
+		if(s < 0 || e < 0 || s >= this.length() || e >= this.length()){throw new IndexOutOfBoundsException();}
+        String g = "";
         for(int q = s; q <= e; q++) {
-            n[q - s] = data[q];
+            g += data[q];
         }
-        CharSequence cs = new CharSequence(n);
-        return cs;
+        return g;
     }
     public int compareTo(CharSequence a) {
+		if(a == null){throw new NullPointerException();}
         int wer = 0;
-        if(a.length() > this.length()) {wer = -1}
+        if(a.length() > this.length()) {wer = -1;}
+		else if(a.length() < this.length()) {wer = 1;}
+		else{wer = 0;}
+		if(wer == -1 || wer == 0) {
+			for(int q = 0; q < this.length(); q++) {
+				if(a.toString().charAt(q) > data[q]){return -1;}
+				else if(a.toString().charAt(q) < data[q]){return 1;}
+				else{q += 0;}
+			}
+			return 0;
+		}
+		else {
+			for(int q = 0; q < a.length(); q++) {
+				if(a.toString().charAt(q) > data[q]){return -1;}
+				else if(a.toString().charAt(q) < data[q]){return 1;}
+				else{q += 0;}
+			}
+			return 0;
+		}
     }
     public MyString(CharSequence s) {
+		if(s.length() <= 0){throw new IndexOutOfBoundsException();}
         data = new char[s.length()];
         for(int q = 0; q < s.length(); q++) {
             data[q] = s.charAt(q);
         }
     }
-    
 }
